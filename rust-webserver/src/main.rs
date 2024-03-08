42a3b2e824766e7cc90e41b3457811d5c47c9e50
LeslieKid <3530611790@qq.com>
2024-03-08 18:36:49 +0800

    multi-threads-webserver

--- rust-webserver/src/main.rs
+++ rust-webserver/src/main.rs
@@ -1,7 +1,5 @@
 use std::{
-    fs,
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
+    fs, io::{prelude::*, BufReader}, net::{TcpListener, TcpStream}, thread, time::Duration
 };
 
 fn main() {
@@ -9,7 +7,9 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         
-        handle_connection(stream); 
+        thread::spawn(move || {
+            handle_connection(stream); 
+        });
     }
 }
 
@@ -22,7 +22,11 @@ fn handle_connection(mut stream: TcpStream) {
     let (status_line, filename) = match request_line.as_str() {
         "GET / HTTP/1.1" => {
             ("HTTP/1.1 200 OK", "hello.html")
-        } 
+        }
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(10));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
         _ => {
             ("HTTP/1.1 404 NOT FOUND", "404.html")
         }
