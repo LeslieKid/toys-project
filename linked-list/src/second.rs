10fc501b9a56ef20976bac742e9fba402b62e247
LeslieKid <3530611790@qq.com>
2024-03-11 14:35:33 +0800

    iterator for linked-list

--- linked-list/src/second.rs
+++ linked-list/src/second.rs
@@ -30,7 +30,6 @@ impl<T> List<T> {
     }
 
     pub fn peek(&self) -> Option<&T> {
-
         self.head.as_ref().map(|node| {
             &node.elem
         })
@@ -38,6 +37,14 @@ impl<T> List<T> {
 
     pub fn into_iter(self) -> IntoIter<T> {
         IntoIter(self)
+    }
+
+    pub fn iter(&self) -> Iter<T> {
+        Iter{ next: self.head.as_deref().map(|node| { &*node }) }
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<T> {
+        IterMut{ next: self.head.as_deref_mut() }
     }
 
 }
@@ -62,8 +69,39 @@ impl<T> Iterator for IntoIter<T> {
     }
 }
 
+/// The concept of lifetime in Rust makes me crazy.
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.elem
+        })
+    }
+}
+
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_deref_mut();
+            &mut node.elem
+        })
+    }
+}
+
 #[cfg(test)]
 mod test {
+    use std::collections::btree_map::IterMut;
+
     use super::List;
 
     #[test]
@@ -107,5 +145,27 @@ mod test {
 
     #[test]
     fn iter_test() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3)); 
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+    }
+
+    #[test]
+    fn iter_mut_test() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter_mut = list.iter_mut();
+        assert_eq!(iter_mut.next(), Some(&mut 3)); 
+        assert_eq!(iter_mut.next(), Some(&mut 2));
+        assert_eq!(iter_mut.next(), Some(&mut 1));
     }
 }
